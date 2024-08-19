90ea5b244a5af984eec61a0c7c4eb7c7fe435c61
Gemechisgk <gemechis.gk@gmail.com>
2024-08-19 14:42:06 +0300

    Car and Driver info added

--- data/travelHistory.js
+++ data/travelHistory.js
@@ -1,28 +1,61 @@
 // travelHistory.js
 const travelHistory = [
     {
-        point: [8.9667, 39.6667], // Adama
+        point: [8.548466, 39.276245], // Adama
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
         speed: 60, // km/h
         fuel: 50 // liters
     },
     {
-        point: [9.0333, 40.0000], // Point along the route
+        point: [8.536413, 39.246376], // Point along the route
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
         speed: 70, // km/h
         fuel: 45 // liters
     },
     {
-        point: [9.0333, 40.2000], // Another point along the route
+        point: [8.538577, 39.217451], // Another point along the route
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
         speed: 65, // km/h
         fuel: 40 // liters
     },
     {
-        point: [9.0333, 40.4000], // Near Addis Ababa
-        speed: 80, // km/h
+        point: [8.552412, 39.200414], // Near Addis Ababa
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
+        speed: 140, // km/h
         fuel: 35 // liters
     },
     {
-        point: [9.03, 38.74], // Addis Ababa
+        point: [8.602857, 39.141742], // Addis Ababa
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
+        speed: 50, // km/h
+        fuel: 30 // liters
+    },
+    {
+        point: [8.862872, 38.825018], // Addis Ababa
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
+        speed: 150, // km/h
+        fuel: 30 // liters
+    },
+    {
+        point: [8.855404, 38.803927], // Addis Ababa
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
+        speed: 70, // km/h
+        fuel: 30 // liters
+    },
+    {
+        point: [8.858096, 38.779911], // Addis Ababa
+        driver:"Abebe Ketema",
+        Vehicle:"Toyota Land Cruiser",
         speed: 50, // km/h
         fuel: 30 // liters
     }
+
+
 ];
